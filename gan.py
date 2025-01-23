3e3c6f0b7665b0f6c2056bd202257ced381d7d79
cxycode32 <cxycode32@gmail.com>
2025-01-23 11:26:28 +0800

    DCGAN -> WGAN

--- gan.py
+++ gan.py
@@ -6,41 +6,48 @@ import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
-from utils import DEVICE, LOG_DIR, clear_directories, check_gradients, check_dead_neurons, plot_losses, save_fake_images, create_gif, save_checkpoint
+from tqdm import tqdm
+from utils import DEVICE, LOG_DIR, initialize_weights, gradient_penalty, clear_directories, check_gradients, check_dead_neurons, plot_losses, save_fake_images, create_gif, save_checkpoint
 
 
 # Hyperparams
 IMAGE_SIZE = 64
 IMAGE_CHANNELS = 1
 NOISE_DIM = 100
-DISCRIMINATOR_FEATURES = 64
-GENERATOR_FEATURES = 64
-BATCH_SIZE = 128
-EPOCH_NUM = 5
+CRITIC_FEATURES = 16
+GENERATOR_FEATURES = 16
+BATCH_SIZE = 64
+EPOCH_NUM = 10
+CRITIC_ITERATIONS = 5
+LAMBDA_GP = 10
 
 # Here are different sets of learning rates for you to experiment with
-# Balanced Training
-DISCRIMINATOR_LEARNING_RATE = 2e-4
-GENERATOR_LEARNING_RATE = 2e-4
+# Set 1: Weaker Balanced Training
+CRITIC_LEARNING_RATE = 1e-4
+GENERATOR_LEARNING_RATE = 1e-4
 
-# Set 2: Stronger Generator, Weaker Discriminator
-# DISCRIMINATOR_LEARNING_RATE = 1e-4
-# GENERATOR_LEARNING_RATE = 5e-4
-
-# Set 3: Stronger Discriminator, Weaker Generator
-# DISCRIMINATOR_LEARNING_RATE = 5e-4
-# GENERATOR_LEARNING_RATE = 1e-4
-
-# Set 4: More Discriminator Updates (Weaker Generator)
-# DISCRIMINATOR_LEARNING_RATE = 3e-4
+# Set 2: Stronger Balanced Training
+# CRITIC_LEARNING_RATE = 2e-4
 # GENERATOR_LEARNING_RATE = 2e-4
 
-# Set 5: Less Discriminator Updates (Weaker Discriminator)
-# DISCRIMINATOR_LEARNING_RATE = 1e-4
+# Set 3: Weaker Critic, Stronger Generator
+# CRITIC_LEARNING_RATE = 1e-4
+# GENERATOR_LEARNING_RATE = 5e-4
+
+# Set 4: Stronger Critic, Weaker Generator
+# CRITIC_LEARNING_RATE = 5e-4
+# GENERATOR_LEARNING_RATE = 1e-4
+
+# Set 5: More Critic Updates (Weaker Generator)
+# CRITIC_LEARNING_RATE = 3e-4
+# GENERATOR_LEARNING_RATE = 2e-4
+
+# Set 6: Less Critic Updates (Weaker Critic)
+# CRITIC_LEARNING_RATE = 1e-4
 # GENERATOR_LEARNING_RATE = 3e-4
 
-# Set 6: Aggressive Training for Both
-# DISCRIMINATOR_LEARNING_RATE = 5e-4
+# Set 7: Aggressive Training for Both
+# CRITIC_LEARNING_RATE = 5e-4
 # GENERATOR_LEARNING_RATE = 5e-4
 
 
@@ -56,12 +63,12 @@ class Discriminator(nn.Module):
             self._block(disc_features * 4, disc_features * 8, 4, 2, 1),
 
             nn.Conv2d(disc_features * 8, 1, kernel_size=4, stride=2, padding=0),
-            nn.Sigmoid(),
         )
         
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.InstanceNorm2d(out_channels, affine=True),
             nn.LeakyReLU(0.2),
         )
 
@@ -79,7 +86,7 @@ class Generator(nn.Module):
             self._block(gen_features * 4, gen_features * 2, 4, 2, 1),
 
             nn.ConvTranspose2d(gen_features * 2, img_channels, kernel_size=4, stride=2, padding=1),
-            nn.Tanh(),  # Normalize images to [-1, 1]
+            nn.Tanh(),
         )
     
     def _block(self, in_channels, out_channels, kernel_size, stride, padding):
@@ -93,7 +100,7 @@ class Generator(nn.Module):
         return self.gen(x)
 
 
-def train_gan(discriminator, generator, fixed_noise, loader, disc_opt, gen_opt, criterion, writer_fake, writer_real, writer_loss):
+def train_dcgan(discriminator, generator, fixed_noise, loader, disc_opt, gen_opt, criterion, writer_fake, writer_real, writer_loss):
     step = 0
     disc_losses, gen_losses = [], []
     
@@ -182,12 +189,96 @@ def train_gan(discriminator, generator, fixed_noise, loader, disc_opt, gen_opt, 
         save_fake_images(generator, fixed_noise, epoch)
                     
     plot_losses(disc_losses, gen_losses)
+    
+    
+def train_wgan(critic, generator, fixed_noise, loader, opt_critic, opt_gen, writer_fake, writer_real, writer_loss):
+    step = 0
+    critic_losses, gen_losses = [], []
+    
+    critic.train()
+    generator.train()
+    
+    for epoch in range(EPOCH_NUM):
+        critic_loss_epoch = 0.0
+        gen_loss_epoch = 0.0
+        
+        for batch_idx, (real, _) in enumerate(tqdm(loader)):
 
+            """
+            Critic Training
 
-def initialize_weights(model):
-    for m in model.modules():
-        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
-            nn.init.normal_(m.weight.data, 0.0, 0.02)
+            The critic learns by:
+            1. Getting real images -> predict 1
+            2. Getting fake images -> predict 0
+            3. Update weights to improve accuracy
+            """
+            
+            real = real.to(DEVICE)
+            cur_batch_size = real.shape[0]
+            
+            for _ in range(CRITIC_ITERATIONS):
+                noise = torch.randn(cur_batch_size, NOISE_DIM, 1, 1).to(DEVICE)
+                fake = generator(noise)  # Create fake images
+            
+                critic_real = critic(real).reshape(-1)  # Critic prediction for real images
+                critic_fake = critic(fake).reshape(-1)  # Critic prediction for fake images
+
+                gp = gradient_penalty(critic, real, fake)
+
+                critic_loss = (-(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp)
+                critic_loss_epoch += critic_loss.item()
+
+                critic.zero_grad()
+                critic_loss.backward(retain_graph=True)
+                opt_critic.step()
+            
+            
+            """
+            Generator Training
+
+            The generator learns by:
+            1. Trying to fool the discriminator to predict 1 for fake images.
+            """
+
+            gen_fake = critic(fake).reshape(-1)
+            gen_loss = -torch.mean(gen_fake)
+            gen_loss_epoch += gen_loss.item()
+            
+            generator.zero_grad()
+            gen_loss.backward()
+            opt_gen.step()
+            
+            if batch_idx % 100 == 0:
+                print(f"EPOCH[{epoch}/{EPOCH_NUM}], "
+                    f"BATCH[{batch_idx}/{len(loader)}]"
+                    f"CRITIC LOSS: {critic_loss:.2f}, "
+                    f"GEN LOSS: {gen_loss:.2f}"
+                )
+                
+                with torch.no_grad():
+                    fake = generator(fixed_noise)
+
+                    img_grid_fake = torchvision.utils.make_grid(fake[:32], normalize=True)
+                    img_grid_real = torchvision.utils.make_grid(real[:32], normalize=True)
+
+                    writer_fake.add_image("Fake", img_grid_fake, global_step=step)
+                    writer_real.add_image("Real", img_grid_real, global_step=step)
+
+                step += 1
+                
+        avg_disc_loss = critic_loss_epoch / len(loader)
+        avg_gen_loss = gen_loss_epoch / len(loader)
+        critic_losses.append(avg_disc_loss)
+        gen_losses.append(avg_gen_loss)
+                    
+        if epoch % 10 == 0:
+            save_checkpoint(epoch, generator, critic, opt_gen, opt_critic)
+            check_gradients(generator)
+            check_gradients(critic)
+                    
+        save_fake_images(generator, fixed_noise, epoch)
+                    
+    plot_losses(critic_losses, gen_losses)
 
 
 def gan():
@@ -206,15 +297,17 @@ def gan():
     dataset = datasets.MNIST(root="mnist_dataset/", transform=transform, train=True, download=True)
     loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-    discriminator = Discriminator(IMAGE_CHANNELS, DISCRIMINATOR_FEATURES).to(DEVICE)
-    initialize_weights(discriminator)
+    # Critic in WGAN is equivalent to Discriminator in normal GAN
+    # It goes by the name because it no longer outputs between [0, 1]
+    critic = Discriminator(IMAGE_CHANNELS, CRITIC_FEATURES).to(DEVICE)
+    initialize_weights(critic)
 
     generator = Generator(NOISE_DIM, IMAGE_CHANNELS, GENERATOR_FEATURES).to(DEVICE)
     initialize_weights(generator)
     
-    disc_opt = optim.Adam(discriminator.parameters(), lr=DISCRIMINATOR_LEARNING_RATE, betas=(0.5, 0.999))
-    gen_opt = optim.Adam(generator.parameters(), lr=GENERATOR_LEARNING_RATE, betas=(0.5, 0.999))
-    criterion = nn.BCELoss()  # Used to measure how well it distinguishes real from fake
+    opt_critic = optim.Adam(critic.parameters(), lr=CRITIC_LEARNING_RATE, betas=(0.0, 0.9))
+    opt_gen = optim.Adam(generator.parameters(), lr=GENERATOR_LEARNING_RATE, betas=(0.0, 0.9))
+    # criterion = nn.BCELoss()  # Used to measure how well it distinguishes real from fake
 
     fixed_noise = torch.randn(32, NOISE_DIM, 1, 1).to(DEVICE)
 
@@ -222,7 +315,8 @@ def gan():
     writer_real = SummaryWriter(f"{LOG_DIR}/real")
     writer_loss = SummaryWriter(f"{LOG_DIR}/losses")
     
-    train_gan(discriminator, generator, fixed_noise, loader, disc_opt, gen_opt, criterion, writer_fake, writer_real, writer_loss)
+    # train_dcgan(discriminator, generator, fixed_noise, loader, opt_critic, opt_gen, criterion, writer_fake, writer_real, writer_loss)
+    train_wgan(critic, generator, fixed_noise, loader, opt_critic, opt_gen, writer_fake, writer_real, writer_loss)
 
     create_gif()
 
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import torch
+import torch.nn as nn
 import shutil
 import imageio
 import torchvision
@@ -11,14 +12,39 @@ MODEL_DIR = "checkpoints"
 ASSETS_DIR = "assets"
 IMAGE_DIR = "fake_images"
 LOG_DIR = "logs"
+directories=[MODEL_DIR, ASSETS_DIR, IMAGE_DIR, LOG_DIR]
 
 
-def clear_directories(directories=[MODEL_DIR, ASSETS_DIR, IMAGE_DIR, LOG_DIR]):
-    """Helper function to clear directories
+def initialize_weights(model):
+    for m in model.modules():
+        if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
+            nn.init.normal_(m.weight.data, 0.0, 0.02)
 
-    Args:
-        directories (list): Directories you want to clear.
-    """
+
+def gradient_penalty(critic, real, fake, device=DEVICE):
+    BATCH_SIZE, C, H, W = real.shape
+    alpha = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated_images = real * alpha + fake * (1 - alpha)
+
+    # Calculate critic scores
+    mixed_scores = critic(interpolated_images)
+
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
+
+
+def clear_directories():
+    """Helper function to clear directories."""
     for directory in directories:
         if os.path.exists(directory):
             shutil.rmtree(directory)
